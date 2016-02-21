b4c48cd5d5ed10d1ad7910a8b75d2533634d9790
Joakim Tillas <joakim@tillas.net>
2016-02-21 14:07:15 +0100

    zz

--- mylib/IndexedFile.hpp
+++ mylib/IndexedFile.hpp
@@ -4,214 +4,209 @@
 #include <string>
 #include <stdexcept>
 #include <unordered_map>
-#include <iostream>
 
-namespace SEB {
-    namespace db {
+namespace seb {
+    namespace dtype {
+        using namespace std;
 
-		// System API functions
-		extern "C" {
-			int truncate(const char* path, size_t length);
-		}
-
-		using namespace std;
-
-		enum class OpenMode {
-			in, out, inout
-		};
-
-		template<typename Record>
-		class IndexedFile {
-			constexpr static size_t       SIZE = sizeof(Record);
-			string                        filename;
-			fstream                       file;
-			unordered_map<string, size_t> index;
-			OpenMode					  omode;
-
-			ios_base::openmode translate(const OpenMode& mode) const {
-				ios_base::openmode m = ios_base::binary;
-				switch (mode) {
-				case OpenMode::in:
-					m  |= ios_base::in;
-					break;
-				case OpenMode::out:
-					m |=ios_base::out;
-					break;
-				case OpenMode::inout:
-					m |=ios_base::in | ios_base::out;
-					break;
-				default:
-					throw invalid_argument("Invalid open mode");
-				}
-				return m;
-			}
-
-			void buildIndex() {
-				index.clear();
-				size_t k = 0;
-				for (auto r : (*this)) {
-					index[r.indexKey()] = k++;
-				}
-			}
-
-			/**
-			 * Moves a single record
-			 */
-			void move(size_t to, size_t from) {
-				char buf[SIZE];
-				file.seekg(from * SIZE);
-				file.read(reinterpret_cast<char*>(buf), SIZE);
-				file.seekp(to * SIZE);
-				file.write(reinterpret_cast<const char*>(buf), SIZE);
-			}
-
-			/**
-			 * Shifts down all records one step, starting from and overwriting the record at pos.
-			 */
-			void shiftDown(size_t pos) {
-				const auto N = count() - 1;
-				for (auto  k = pos; k < N; ++k) {
-					move(k, k+1);
-				}
-			}
-
-			static void read(fstream& db, size_t pos, Record& r) {
-				db.seekg(pos * SIZE);
-				db.read(reinterpret_cast<char*>(&r), sizeof(Record));
-			}
-
-			static void write(fstream& db, size_t pos, const Record& r) {
-				db.seekp(pos * SIZE);
-				db.write(reinterpret_cast<const char*>(&r), sizeof(Record));
-			}
-
-			void append(fstream& db, const Record& r) {
-				db.seekp(0, ios_base::end);
-				db.write(reinterpret_cast<const char*>(&r), sizeof(Record));
-			}
-
-		public:
-			IndexedFile(const string filename, OpenMode mode) : filename{filename} {
-				omode = mode;
-				file.open(filename, translate(omode));
-				if (!file) throw invalid_argument("cannot open " + filename);
-			}
-
-			~IndexedFile() = default;
-			IndexedFile() = delete;
-			IndexedFile(const IndexedFile<Record>&) = delete;
-			IndexedFile<Record>& operator=(const IndexedFile<Record>&) = delete;
+        // System API functions
+        extern "C" {
+        int truncate(const char* path, size_t length);
+        }
 
 
-			size_t size() {
-				file.seekg(0, ios_base::end);
-				return static_cast<size_t>(file.tellg());
-			}
+        enum class OpenMode {
+            in, out, inout
+        };
 
-			unsigned count() {
-				return this->size() / SIZE;
-				//...divide the file-size with the record-size
-			}
+        template<typename Record>
+        class IndexedFile {
+            constexpr static size_t       SIZE = sizeof(Record);
+            string                        filename;
+            fstream                       file;
+            unordered_map<string, size_t> index;
+            OpenMode					  omode;
 
-			/**
-			 * Appends a record to the end of the file
-			 */
-			IndexedFile<Record>& operator<<(const Record& r) {
-				if(omode == OpenMode::in)
-					throw invalid_argument("file not open for writing");
-				append(file, r);
+            ios_base::openmode translate(const OpenMode& mode) const {
+                ios_base::openmode m = ios_base::binary;
+                switch (mode) {
+                    case OpenMode::in:
+                        m  |= ios_base::in;
+                        break;
+                    case OpenMode::out:
+                        m |=ios_base::out;
+                        break;
+                    case OpenMode::inout:
+                        m |=ios_base::in | ios_base::out;
+                        break;
+                    default:
+                        throw invalid_argument("Invalid open mode");
+                }
+                return m;
+            }
+
+            void buildIndex() {
+                index.clear();
+                size_t k = 0;
+                for (auto r : (*this)) {
+                    index[r.indexKey()] = k++;
+                }
+            }
+
+            /**
+             * Moves a single record
+             */
+            void move(size_t to, size_t from) {
+                char buf[SIZE];
+                file.seekg(from * SIZE);
+                file.read(reinterpret_cast<char*>(buf), SIZE);
+                file.seekp(to * SIZE);
+                file.write(reinterpret_cast<const char*>(buf), SIZE);
+            }
+
+            /**
+             * Shifts down all records one step, starting from and overwriting the record at pos.
+             */
+            void shiftDown(size_t pos) {
+                const auto N = count() - 1;
+                for (auto  k = pos; k < N; ++k) {
+                    move(k, k+1);
+                }
+            }
+
+            static void read(fstream& db, size_t pos, Record& r) {
+                db.seekg(pos * SIZE);
+                db.read(reinterpret_cast<char*>(&r), sizeof(Record));
+            }
+
+            static void write(fstream& db, size_t pos, Record& r) {
+                db.seekp(pos * SIZE);
+                db.write(reinterpret_cast<const char*>(&r), sizeof(Record));
+            }
+
+            static void append(fstream& db, Record& r) {
+                db.seekp(0, ios_base::end);
+                db.write(reinterpret_cast<const char*>(&r), sizeof(Record));
+            }
+
+        public:
+            IndexedFile(const string filename, OpenMode mode) : filename{filename} {
+                omode = mode;
+                file.open(filename, translate(omode));
+                if (!file) throw invalid_argument("cannot open " + filename);
+            }
+
+            ~IndexedFile() = default;
+            IndexedFile() = delete;
+            IndexedFile(const IndexedFile<Record>&) = delete;
+            IndexedFile<Record>& operator=(const IndexedFile<Record>&) = delete;
 
 
-				return *this;
-			}
+            size_t size() {
+                file.seekg(0, ios_base::end);
+                return static_cast<size_t>(file.tellg());
+            }
 
-			/**
-			 * Used for for-each and operator[]
-			 */
-			struct iterator {
-				fstream&  db;
-				size_t    pos;
+            unsigned count() {
+                return this->size() / SIZE;
+            }
 
-				iterator(fstream& db, size_t pos) : db{db}, pos{pos} { }
+            /**
+             * Appends a record to the end of the file
+             */
+            IndexedFile<Record>& operator<<(const Record& r) {
+                if(omode == OpenMode::in)
+                    throw invalid_argument("file not open for writing");
 
-				bool operator!=(const iterator& that) {
-					return this->pos < that.pos;
-				}
+                append(file, r);
+                return *this;
+            }
 
-				void operator++() {
-					this->pos++;
-				}
+            /**
+             * Used for for-each and operator[]
+             */
+            struct iterator {
+                fstream& db;
+                size_t pos;
 
-				Record operator*() {
-					Record r;
-					read(db, pos, r);
-					return r;
-				}
+                iterator(fstream& db, size_t pos) : db{db}, pos{pos} { }
 
-				operator Record() {
-					return operator*();
-				}
+                bool operator!=(const iterator& that) {
+                    return this->pos < that.pos;
+                }
 
-				void operator=(const Record& r) {
-					write(db, pos, r);
-				}
-			};
+                void operator++() {
+                    this->pos++;
+                }
 
-			iterator begin() { return {file, 0}; }
-			iterator end()   { return {file, count()}; }
+                Record operator*() {
+                    Record r;
+                    read(db, pos, r);
+                    return r;
+                }
+
+                operator Record() {
+                    return operator*();
+                }
+
+                void operator=(const Record& r) {
+                    write(db, pos, r);
+                }
+            };
+
+            iterator begin() { return {file, 0}; }
+            iterator end() { return {file, count()}; }
 
 
-			iterator operator[](size_t pos) {
-				if (pos > count()) {
-					throw out_of_range("index too large - current size is " + to_string(count()));
-				}
-				return {file, pos};
-			}
+            iterator operator[](size_t pos) {
+                if (pos > count()) {
+                    throw out_of_range("index too large - current size is " + to_string(count()));
+                }
+                return {file, pos};
+            }
 
+            iterator operator[](const string& idx) {
+                if (index.empty() && count() > 0) {
+                    buildIndex();
+                }
 
+                if (index.count(idx) == 0) {
+                    throw out_of_range("no index for " + idx + " found");
+                }
 
-			iterator operator[](const string& idx) {
-				if (index.empty() && count() > 0) {
-					buildIndex();
-				}
+                return (*this)[index[idx]];
+            }
 
-				if (index.count(idx) == 0) {
-					throw out_of_range("no index for " + idx + " found");
-				}
+            void erase(size_t pos) {
+                const auto N = count();
+                if (N == 0) {
+                    throw out_of_range("can not erase, database empty");
+                }
 
-				return (*this)[index[idx]];
-			}
+                if (pos > N) {
+                    throw out_of_range("index too large - current size is " + to_string(count()));
+                }
 
-			void erase(size_t pos) {
-				const auto N = count();
-				if (N == 0) {
-					throw out_of_range("can not erase, database empty");
-				}
+                shiftDown(pos);
 
-				if (pos > N) {
-					throw out_of_range("index too large - current size is " + to_string(count()));
-				}
+                truncate(filename.c_str(), (N - 1) * SIZE); //adjusts the file size
 
-				shiftDown(pos);
+                index.clear();
+            }
 
-				truncate(filename.c_str(), (N - 1) * SIZE); //adjusts the file size
+            void erase(const string& idx) {
+                if (index.empty() && count() > 0) {
+                    throw out_of_range("can not erase, database empty");
+                }
 
-				index.clear();
-			}
+                if (index.count(idx) == 0) {
+                    throw out_of_range("no index for " + idx + " found");
+                }
 
-			void erase(const string& idx) {
-				if (index.empty() && count() > 0) {
-					throw out_of_range("can not erase, database empty");
-				}
+                erase(index[idx]);
+            }
 
-				if (index.count(idx) == 0) {
-					throw out_of_range("no index for " + idx + " found");
-				}
+        };
 
-				erase(index[idx]);
-			}
-
-		};
     }
-
 }
